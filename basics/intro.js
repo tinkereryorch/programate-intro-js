6efb150d96589ef56e706fe72058a902b25e338e
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-11 23:21:16 -0500

    Added function examples

--- basics/intro.js
+++ basics/intro.js
@@ -16,4 +16,44 @@ console.log(unNumero + otroNumero);
 // Declarar un arreglo
 var myArray = [3, 6, 9, 12];
 
+// Hay diferentes maneras de llamar a una función
+function duplicarArreglo(theArray) {
+    let helperArray = [];
+    
+    for(let i = 0; i < theArray.length; i++) {
+        helperArray.push(theArray[i] * 2);
+    }
 
+    return helperArray;
+}
+
+const duplicarArreglo2 = function (theArray) {
+    let helperArray = [];
+    
+    for(let i = 0; i < theArray.length; i++) {
+        helperArray.push(theArray[i] * 2);
+    }
+
+    return helperArray;
+}
+
+const duplicarArreglo3 = (theArray) => {
+    let helperArray = [];
+    
+    for(let i = 0; i < theArray.length; i++) {
+        helperArray.push(theArray[i] * 2);
+    }
+
+    return helperArray;
+}
+
+let result1 = duplicarArreglo(myArray);
+let result2 = duplicarArreglo2(myArray);
+let result3 = duplicarArreglo3(myArray);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+
+let result4 = myArray.map((item) => {return item * 2});
+console.log(result4);
